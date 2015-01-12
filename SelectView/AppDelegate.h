7b01c82a43dffcb27986189cd70ca91c0c520668
kt <kt@a2ndeiMac.local>
2015-01-12 16:43:41 +0800

    Initial Commit

---
+++ SelectView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectView
+//
+//  Created by kt on 15/1/12.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelectView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelectView
+//
+//  Created by kt on 15/1/12.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
